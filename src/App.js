93d6086f8504a5e98a4009c1fdf9649e3366f5b4
priyankRaiyani19 <priyankraiyani19@gmail.com>
2025-01-07 11:22:28 +0530

    add browser router and Home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+import {Route,Routes} from "react-router-dom";
 
 function App() {
     return (
-        <main className="flex justify-center gap-4 flex-col min-h-screen">
-            <h1 className="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-            <p className="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-        </main>
+     <div>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+
+
+        </Routes>
+     </div>
     );
 }
 
