f5666e33667d3f227b91748aac145e9263b3f923
priyankRaiyani19 <priyankraiyani19@gmail.com>
2025-01-10 09:11:13 +0530

    add login and sign up UI

--- src/components/core/HomePage/ExploreMore.js
+++ src/components/core/HomePage/ExploreMore.js
@@ -1,83 +1,79 @@
-import React from 'react'
-import {HomePageExplore} from "../../../data/homepage-explore"
-import HighlightText from './HighlightText';
-import {useState} from 'react';
-import CourseCard from './CourseCard';
+import React, { useState } from "react";
+import { HomePageExplore } from "../../../data/homepage-explore";
+import CourseCard from "./CourseCard";
+import HighlightText from "./HighlightText";
 
 const tabsName = [
-
-    "Free", "New To Coding", "Most Popular", "Skill Path", "Carrer Path"]
+    "Free",
+    "New to coding",
+    "Most popular",
+    "Skills paths",
+    "Career paths",
+];
 
 const ExploreMore = () => {
-
     const [currentTab, setCurrentTab] = useState(tabsName[0]);
     const [courses, setCourses] = useState(HomePageExplore[0].courses);
-    const [currentCard, setCurrentCard] = useState(HomePageExplore[0].courses[0].heading);
+    const [currentCard, setCurrentCard] = useState(
+        HomePageExplore[0].courses[0].heading
+    );
 
-
-    const setMyCard = (value) => {
+    const setMyCards = (value) => {
         setCurrentTab(value);
-        const result = HomePageExplore.filter((course) => (course.tag === value));
+        const result = HomePageExplore.filter((course) => course.tag === value);
         setCourses(result[0].courses);
         setCurrentCard(result[0].courses[0].heading);
-    }
+    };
 
-    return (<div className="   ">
+    return (
+        <div>
+            {/* Explore more section */}
+            <div>
+                <div className="text-4xl font-semibold md:text-center my-10">
+                    Unlock the
+                    <HighlightText text={"Power of Code"} />
+                    <p className="md:text-center text-richblack-300 text-lg font-semibold mt-1">
+                        Learn to Build Anything You Can Imagine
+                    </p>
+                </div>
+            </div>
 
-        {/*? heading*/}
+            {/* Tabs Section */}
+            <div className="hidden lg:flex gap-5 -mt-5 mx-auto w-max bg-richblack-800 text-richblack-200 p-1 rounded-full font-medium drop-shadow-[0_1.5px_rgba(255,255,255,0.25)]">
+                {tabsName.map((ele, index) => {
+                    return (
+                        <div
+                            className={` text-[16px] flex flex-row items-center gap-2 ${
+                                currentTab === ele
+                                    ? "bg-richblack-900 text-richblack-5 font-medium"
+                                    : "text-richblack-200"
+                            } px-7 py-[7px] rounded-full transition-all duration-200 cursor-pointer hover:bg-richblack-900 hover:text-richblack-5`}
+                            key={index}
+                            onClick={() => setMyCards(ele)}
+                        >
+                            {ele}
+                        </div>
+                    );
+                })}
+            </div>
+            <div className="hidden lg:block lg:h-[200px]"></div>
 
-        <div className="lg:text-center  text-4xl font-semibold mt-7">
-            Unlock The
-            <HighlightText text={"Power Of Code"}/>
+            {/* Cards Group */}
+            <div className="lg:absolute gap-10 justify-center lg:gap-0 flex lg:justify-between flex-wrap w-full lg:bottom-[0]
+            lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[50%]  text-black lg:mb-0 mb-[-25%] lg:px-0 px-3">
+                {courses.map((ele, index) => {
+                    return (
+                        <CourseCard
+                            key={index}
+                            cardData={ele}
+                            currentCard={currentCard}
+                            setCurrentCard={setCurrentCard}
+                        />
+                    );
+                })}
+            </div>
         </div>
+    );
+};
 
-        <div className=' mt-3 md:w-[90%] w-[100%] md:text-center text-lg  text-richblack-300'>
-
-            Learn to Build Anything You Can Imagine
-        </div>
-
-        {/*  ? Tab portion  */}
-
-        <div className=" flex  flex-row rounded-full  bg-richblack-700 mt-8 px-1 py-1 ">
-
-            {tabsName.map((ele, index) => {
-                return (<div key={index}
-                             className={`text-[16px] flex flex-row items-center gap-3  
-                             ${currentTab === ele ? "bg-richblack-900 text-richblack-5 font-medium"
-                                 : " bg-richblack-700 text-richblack-100 font-thin"} 
-                            rounded-full duration-200 cursor-pointer hover:bg-richblack-900 px-7  py-2`}
-                             onClick={() => setMyCard(ele)}
-
-                >
-                    {ele}
-                </div>)
-            })
-
-            }
-
-        </div>
-
-        <div className={`md:h-[150px] `}></div>
-
-        {/* ? card component */}
-
-        {/* * group of cards*/}
-
-        <div className={`absolute flex flex-row justify-between w-full gap-10`}>
-            {
-
-                courses.map((ele, index) => {
-
-                    <CourseCard
-                        key={index}
-                        cardData={ele}
-                        currentCard={currentCard}
-                        currentCard={setCurrentCard}/>
-                })
-            }
-        </div>
-    </div>)
-}
-
-
-export default ExploreMore
+export default ExploreMore;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
 
 
                     <div className='ml-0 md:ml-auto group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200
-            transition-all duration-200 hover:scale-95 w-fit'>
+            transition-all duration-200 hover:scale-95 w-fit shadow-[rgba(120,120,120,120)_1.5px_1.5px_2px_0px]'>
                         <Link to={"/signup"}>
                         <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px]
                 transition-all duration-200 group-hover:bg-richblack-900'>
