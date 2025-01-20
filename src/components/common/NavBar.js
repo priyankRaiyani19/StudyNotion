7f27399835e4f844f2800933f796e7f6057f1034
priyankRaiyani19 <priyankraiyani19@gmail.com>
2025-01-20 10:22:08 +0530

    nav bar

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link,matchPath} from "react-router-dom";
+import {Link, matchPath} from "react-router-dom";
 import Logo from '../../assets/Logo/Logo-Full-Light.png';
 import {NavbarLinks} from '../../data/navbar-links'
 import {IoIosArrowDown} from "react-icons/io";
@@ -9,60 +9,59 @@ const NavBar = () => {
     // ! ***************************************
     const location = useLocation('');
     const matchRoute = (route) => {
-        return matchPath({path: route},location.pathname)
+        return matchPath({path: route}, location.pathname)
     }
 
 
     return (
         <div className="flex w-[100%] items-center justify-center h-14 border-b-[1px] border-b-richblack-600">
 
-        <div className={`flex flex-row items-center justify-between max-w-maxContent w-11/12 rounded-full `}>
+            <div className={`flex flex-row items-center justify-between max-w-maxContent w-11/12 rounded-full `}>
 
-            {/* ? image add*/}
+                {/* ? image add*/}
 
-            <Link to="/">
-                <img src={Logo} alt="not found" width="160" height="42" loading={"lazy"}/>
-            </Link>
+                <Link to="/">
+                    <img src={Logo} alt="not found" width="160" height="42" loading={"lazy"}/>
+                </Link>
 
-            {/* ?  Nav links add  */}
+                {/* ?  Nav links add  */}
 
-            <nav>
-                <ul className={`flex flex-row items-center justify-between gap-x-6 text-[14px] text-richblack-25 `}>
-                    {NavbarLinks.map((ele, index) => {
-                        // in this we need return if we write {} this brackets
-                        return (<li key={index}>
-                            {ele.title === "Catalog" ? (
-                                <div className={`flex flex-row w-full justify-center items-center gap-1`}>
-                                    <div>
-                                        {ele?.title}
-                                    </div>
-                                    <div>
-                                        <IoIosArrowDown/>
-                                    </div>
-                                </div>)
-                                : (<Link to={ele?.path}>
+                <nav>
+                    <ul className={`flex flex-row items-center justify-between gap-x-6 text-[14px] text-richblack-25 `}>
+                        {NavbarLinks.map((ele, index) => {
+                            // in this we need return if we write {} this brackets
+                            return (<li key={index}>
+                                {ele.title === "Catalog" ? (
+                                        <div className={`flex flex-row w-full justify-center items-center gap-1`}>
+                                            <div>
+                                                {ele?.title}
+                                            </div>
+                                            <div>
+                                                <IoIosArrowDown/>
+                                            </div>
+                                        </div>)
+                                    : (<Link to={ele?.path}>
 
-                                <div  className={`${matchRoute(ele.path) ?  "text-yellow-5 font-semibold" : "text-richblack-25"}`}>
-                                    {ele?.title}
-                                </div>
+                                        <div
+                                            className={`${matchRoute(ele.path) ? "text-yellow-5 font-semibold" : "text-richblack-25"}`}>
+                                            {ele?.title}
+                                        </div>
 
-                            </Link>)}
-                        </li>);
-                    })}
-                </ul>
-            </nav>
+                                    </Link>)}
+                            </li>);
+                        })}
+                    </ul>
+                </nav>
 
-        {/* ? buttons : login signUp and dashboard */}
+                {/* ? buttons : login signUp and dashboard */}
 
-            <div className={`flex flex-row items-center gap-x-4`}>
+                <div className={`flex flex-row items-center gap-x-4`}>
+
+                </div>
+
 
             </div>
-
-
-
-
-        </div>
-    </div>);
+        </div>);
 };
 
 export default NavBar;
